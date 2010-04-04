6d8aee8e3738e2617c76a249f9423c4f3579369f
Javier Jardón <jjardon@gnome.org>
2010-04-04 17:14:51 +0200

    [libgiggle] Improve performance: Use G_GNUC_CONST
    Use G_GNUC_CONST in giggle_ref_get_type() function

--- libgiggle/giggle-ref.h
+++ libgiggle/giggle-ref.h
@@ -46,7 +46,7 @@ struct _GiggleRefClass {
 	GObjectClass parent_class;
 };
 
-GType                   giggle_ref_get_type          (void);
+GType                   giggle_ref_get_type          (void) G_GNUC_CONST;
 GiggleRef             * giggle_ref_new               (const gchar *name);
 
 G_CONST_RETURN gchar  * giggle_ref_get_name          (GiggleRef   *ref);
