7f227e2a9bf40af1b0ff3fc4f4720d3d6bb03563
Javier Jardón <jjardon@gnome.org>
2010-04-08 00:35:17 +0200

    [test] Make GtkBuilder validation fail if xmllint is not installed

--- test/test_gtkbuilder_file_validation.sh
+++ test/test_gtkbuilder_file_validation.sh
@@ -1,8 +1,14 @@
 #/bin/sh -e
 
-for x in $(find ../data/ -name "*.ui")
+which xmllint >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "xmllint not installed. Please install it."
+  exit 0
+fi
+
+for x in `find ../data/ -name "*.ui"`
 do
-  # echo Validating $x
   # TODO: Is there a GtkBuilder DTD?
+  echo Validating $x
   xmllint --noout $x || exit 1
 done
